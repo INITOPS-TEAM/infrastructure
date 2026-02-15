b97265f0deae0b93029a09546349c3aa852271ca
pmv-pmv <pisarenkomariia@gmail.com>
2026-02-15 17:41:13 +0200

    fix:INIT-83 add columns to the images table

--- ansible/roles/db/files/schema.sql
+++ ansible/roles/db/files/schema.sql
@@ -63,3 +63,6 @@ CREATE TABLE IF NOT EXISTS banned (
 
 -- ALTER TABLE images ADD COLUMN IF NOT EXISTS location VARCHAR; -- added
 -- ALTER TABLE images DROP COLUMN IF EXISTS original_filename;
+
+ALTER TABLE images ADD COLUMN IF NOT EXISTS location_is_hidden VARCHAR; -- added
+ALTER TABLE images ADD COLUMN IF NOT EXISTS location_password_hash VARCHAR; -- added
